f249e088b7ab2b19564f7d855515326ce6ae26c9
José Valladares <jvalladares@awarewire.net>
2025-04-24 22:59:07 -0400

    Updates to lazy loading
    Implements lazy loading for components in the application.
    
    This improves initial load time by only loading components when they are needed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   {
@@ -13,21 +9,21 @@ export const routes: Routes = [
   {
     path: 'home',
     pathMatch: 'full',
-    component: HomeComponent
+    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent)
   },
   {
     path: 'login',
     pathMatch: 'full',
-    component: LoginComponent
+    loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent)
   },
   {
     path: 'register',
     pathMatch: 'full',
-    component: RegisterComponent
+    loadComponent: () => import('./register/register.component').then((m) => m.RegisterComponent)
   },
   {
     path: 'forgot-password',
     pathMatch: 'full',
-    component: ForgotPasswordComponent
+    loadComponent: () => import('./forgot-password/forgot-password.component').then((m) => m.ForgotPasswordComponent)
   }
 ];
